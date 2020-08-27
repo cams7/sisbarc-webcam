2ca9bc14af78ac401a58b854a29cd94af9db8193
César Magalhães <ceanma@gmail.com>
2020-08-27 15:56:10 -0300

    Algumas funcionalidades comuns de httpd desse modulo sisbarc-webcam foram movidas para o módulo sisbarc-common.

--- main/app_httpd.c
+++ main/app_httpd.c
@@ -16,17 +16,11 @@
 #include "sdkconfig.h"
 
 #include "app_common.h"
-#include "app_httpd.h"
+#include "app_httpd_common.h"
 #include "app_camera.h"
+#include "app_httpd.h"
 
 #pragma GCC diagnostic ignored "-Wincompatible-pointer-types"
-
-#ifdef CONFIG_IDF_TARGET_ESP32
-#define CHIP_NAME "ESP32"
-#endif
-
-#define FILE_PATH_MAX (ESP_VFS_PATH_MAX + 128)
-#define SCRATCH_BUFSIZE (10240)
 
 #define PART_BOUNDARY "123456789000000000000987654321"
 static const char *_STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
@@ -53,11 +47,6 @@ typedef struct {
     httpd_req_t *req;
     size_t len;
 } jpg_chunking_t;
-
-typedef struct rest_server_context {
-    char base_path[ESP_VFS_PATH_MAX + 1];
-    char scratch[SCRATCH_BUFSIZE];
-} rest_server_context_t;
 
 esp_err_t init_server(void) {
 	rest_server_context_t *rest_context = NULL;
@@ -164,34 +153,6 @@ err_init:
 	if(!!rest_context) free(rest_context);
 
 	return ESP_FAIL;
-}
-
-static esp_err_t resp_send_json_data(httpd_req_t *req, cJSON *json_data, const char* httpStatus) {
-	esp_err_t resp;
-	char *data = cJSON_Print(json_data);
-	httpd_resp_set_hdr(req, HTTP_HEAD_ALLOW_ORIGIN, "*");
-	httpd_resp_set_type(req, CONTENT_TYPE_APPLICATION_JSON);
-	httpd_resp_set_status(req, httpStatus);
-	resp = httpd_resp_sendstr(req, data);
-	free(data);
-	return resp;
-}
-
-static esp_err_t resp_send_json_data_ok(httpd_req_t *req, cJSON *json_data) {
-	return resp_send_json_data(req, json_data, _200_OK);
-}
-
-static esp_err_t resp_send_json_message(httpd_req_t *req, const char* httpStatus, const char* message) {
-	cJSON *json_data = cJSON_CreateObject();
-	cJSON_AddStringToObject(json_data, "message", message);
-	esp_err_t resp = resp_send_json_data(req, json_data, httpStatus);
-	cJSON_Delete(json_data);
-	return resp;
-}
-
-static esp_err_t resp_send_json_invalid_content(httpd_req_t *req) {
-	esp_err_t resp = resp_send_json_message(req, _400_BAD_REQUEST, ERR_MSG_INVALID_CONTENT);
-	return resp;
 }
 
 static void get_chip_info(esp_chip_info_t *chip_info, cJSON *resp_json_data) {
@@ -400,92 +361,6 @@ err_capture_with_resp:
 err_capture:
 	if (!!fb) esp_camera_fb_return(fb);
 	return resp;
-}
-
-static char* getBuffer(httpd_req_t *req, esp_err_t *resp) {
-	char *buffer = NULL;
-	int total_len = req->content_len;
-
-	if (total_len >= SCRATCH_BUFSIZE) {
-		*resp = resp_send_json_message(req, _400_BAD_REQUEST, "Content too long");
-		APP_ERROR(err_set_buffer);
-	}
-
-	int cur_len = 0;
-	buffer = ((rest_server_context_t *)(req->user_ctx))->scratch;
-
-	int received = 0;
-
-	while (cur_len < total_len) {
-		received = httpd_req_recv(req, buffer + cur_len, total_len);
-		if (received <= 0) {
-			*resp = resp_send_json_message(req, _500_INTERNAL_SERVER_ERROR, "Failed to post control value");
-			APP_ERROR(err_set_buffer);
-		}
-		cur_len += received;
-	}
-	buffer[total_len] = '\0';
-
-	return buffer;
-err_set_buffer:
-	return NULL;
-}
-
-typedef enum {
-    VAL_BETWEEN, // Validates if a value is between an interval
-    VAL_BOOL,    // Validates if a value is 0 or 1
-} validation_type;
-
-static bool validateBetweenIntVal(cJSON *resp_json_err, const char* attr, const int* val, const int16_t min, const int16_t max) {
-	bool isValid = true;
-
-	if(*val < min || *val > max) {
-		char message[42];
-		sprintf(message, "Value should be between %d and %d", min, max);
-		cJSON_AddStringToObject(resp_json_err, attr, message);
-		isValid = false;
-	}
-
-	return isValid;
-}
-
-static bool validateBoolVal(cJSON *resp_json_err, const char* attr, const int* val) {
-	bool isValid = true;
-
-	if(*val != JSON_ATTR_FALSE && *val != JSON_ATTR_TRUE) {
-		char message[21];
-		sprintf(message, "Value must be %d or %d", JSON_ATTR_FALSE, JSON_ATTR_TRUE);
-		cJSON_AddStringToObject(resp_json_err, attr, message);
-		isValid = false;
-	}
-
-	return isValid;
-}
-
-static int getAttrIntVal(cJSON *req_json_data, cJSON *resp_json_err, const char* attr, validation_type validationType, bool isOk, bool *hasError, const uint8_t total_args, ...) {
-	int val = JSON_GET_INT(req_json_data, attr);
-	if (val != JSON_INT_ATTR_NOTFOUND && isOk) {
-		switch (validationType) {
-			case VAL_BETWEEN: {
-				va_list valist;
-				va_start(valist, total_args);
-				const int min = va_arg(valist, int);
-				const int max = va_arg(valist, int);
-				va_end(valist);
-
-				if(!validateBetweenIntVal(resp_json_err, attr, &val, min, max))
-					*hasError = true;
-				break;
-			}
-			case VAL_BOOL:
-				if(!validateBoolVal(resp_json_err, attr, &val))
-					*hasError = true;
-				break;
-			default:
-				break;
-		}
-	}
-	return val;
 }
 
 static void setSensorIntVal(sensor_t *sensor, cJSON *resp_json_err, cJSON *resp_json_data, const char* attr, int* val, bool *hasError, int (*f)(sensor_t*, int)) {
--- main/include/app_httpd.h
+++ main/include/app_httpd.h
@@ -13,26 +13,6 @@ extern "C" {
 
 #include "esp_err.h"
 
-#define CONTENT_TYPE_TEXT_PLAIN "text/plain"
-#define CONTENT_TYPE_TEXT_HTML  "text/html"
-#define CONTENT_TYPE_TEXT_XML   "text/xml"
-#define CONTENT_TYPE_TEXT_CSS   "text/css"
-#define CONTENT_TYPE_APPLICATION_JSON       "application/json"
-#define CONTENT_TYPE_APPLICATION_JAVASCRIPT "application/javascript"
-#define CONTENT_TYPE_IMAGE_PNG   "image/png"
-#define CONTENT_TYPE_IMAGE_JPEG  "image/jpeg"
-#define CONTENT_TYPE_IMAGE_XICON "image/x-icon"
-
-#define _200_OK                    "200"
-#define _400_BAD_REQUEST           "400"
-#define _500_INTERNAL_SERVER_ERROR "500"
-
-#define HTTP_HEAD_ALLOW_ORIGIN "Access-Control-Allow-Origin"
-
-#define ERR_MSG_SOMETHING_WRONG              "Something wrong"
-#define ERR_MSG_INVALID_CONTENT              "Invalid content"
-#define ERR_MSG_REQ_JSON_DATA_LOADING_BUFFER "An error occurred while loading the buffer"
-
 #define APP_HTTPD_TAG "app_httpd"
 
 esp_err_t init_server(void);
